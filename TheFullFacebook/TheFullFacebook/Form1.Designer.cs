9ce797fca883241f3e872718dcb4e51cad9fab2f
Marinlestylo <chil.tfm@gmail.com>
2019-05-02 08:05:18 +0200

    First Commit
    Commit premier du nom

---
+++ TheFullFacebook/TheFullFacebook/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TheFullFacebook
+{
+    partial class TheFacebook
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TheFacebook));
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pbProgression = new System.Windows.Forms.ProgressBar();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnLike = new System.Windows.Forms.Button();
+            this.btnUnlike = new System.Windows.Forms.Button();
+            this.pbWallpaper = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseImageFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWallpaper)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(363, 448);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(106, 42);
+            this.btnNext.TabIndex = 17;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pbProgression
+            // 
+            this.pbProgression.Location = new System.Drawing.Point(69, 496);
+            this.pbProgression.Name = "pbProgression";
+            this.pbProgression.Size = new System.Drawing.Size(400, 28);
+            this.pbProgression.TabIndex = 16;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(199, 530);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(135, 42);
+            this.btnGenerate.TabIndex = 15;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnLike
+            // 
+            this.btnLike.Location = new System.Drawing.Point(199, 448);
+            this.btnLike.Name = "btnLike";
+            this.btnLike.Size = new System.Drawing.Size(135, 42);
+            this.btnLike.TabIndex = 14;
+            this.btnLike.Text = "I LIKE DAT";
+            this.btnLike.UseVisualStyleBackColor = true;
+            this.btnLike.Click += new System.EventHandler(this.btnLike_Click);
+            // 
+            // btnUnlike
+            // 
+            this.btnUnlike.Location = new System.Drawing.Point(69, 448);
+            this.btnUnlike.Name = "btnUnlike";
+            this.btnUnlike.Size = new System.Drawing.Size(106, 42);
+            this.btnUnlike.TabIndex = 13;
+            this.btnUnlike.Text = "Previous";
+            this.btnUnlike.UseVisualStyleBackColor = true;
+            this.btnUnlike.Click += new System.EventHandler(this.btnUnlike_Click);
+            // 
+            // pbWallpaper
+            // 
+            this.pbWallpaper.Image = ((System.Drawing.Image)(resources.GetObject("pbWallpaper.Image")));
+            this.pbWallpaper.Location = new System.Drawing.Point(69, 27);
+            this.pbWallpaper.Name = "pbWallpaper";
+            this.pbWallpaper.Size = new System.Drawing.Size(400, 400);
+            this.pbWallpaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWallpaper.TabIndex = 12;
+            this.pbWallpaper.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(534, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chooseImageFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // chooseImageFolderToolStripMenuItem
+            // 
+            this.chooseImageFolderToolStripMenuItem.Name = "chooseImageFolderToolStripMenuItem";
+            this.chooseImageFolderToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.chooseImageFolderToolStripMenuItem.Text = "Choose image folder";
+            this.chooseImageFolderToolStripMenuItem.Click += new System.EventHandler(this.ChooseImageFolderToolStripMenuItem_Click);
+            // 
+            // TheFacebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 576);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.pbProgression);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnLike);
+            this.Controls.Add(this.btnUnlike);
+            this.Controls.Add(this.pbWallpaper);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TheFacebook";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TheFacebook";
+            this.Load += new System.EventHandler(this.TheFacebook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWallpaper)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ProgressBar pbProgression;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnLike;
+        private System.Windows.Forms.Button btnUnlike;
+        private System.Windows.Forms.PictureBox pbWallpaper;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chooseImageFolderToolStripMenuItem;
+    }
+}
+
---
+++ TheFullFacebook/TheFullFacebook/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Deployment.Compression.Cab;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheFullFacebook
+{
+    public partial class TheFacebook : Form
+    {
+        private List<string> _likeds;
+        private List<Image> _pictures;
+        private List<string> _picturePath;
+        private string _path;
+        private int _currentPic;
+
+        public TheFacebook()
+        {
+            InitializeComponent();
+        }
+
+        private void TheFacebook_Load(object sender, EventArgs e)
+        {
+            btnGenerate.Enabled = false;
+            btnLike.Enabled = false;
+            btnNext.Enabled = false;
+            btnUnlike.Enabled = false;
+            _currentPic = 0;
+            _likeds = new List<string>();
+            _pictures = new List<Image>();
+            _picturePath = new List<string>();
+        }
+
+        private void ChooseImageFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog choosePicFolder = new FolderBrowserDialog();
+            choosePicFolder.ShowNewFolderButton = false;
+            DialogResult result = choosePicFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _path = choosePicFolder.SelectedPath;
+                if (SelectPic())
+                {
+                    btnGenerate.Enabled = true;
+                    btnLike.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnUnlike.Enabled = true;
+                    ListShuffle();
+                    UpdatePic();
+                }
+            }
+        }
+
+        private void ListShuffle()
+        {
+            Random rng = new Random();
+            _pictures = _pictures.OrderBy(p => rng.Next()).ToList();
+        }
+
+        private bool SelectPic()
+        {
+            List<string> imagesList = Directory.GetFiles(_path, "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+            imagesList.AddRange(Directory.GetFiles(_path, "*.png", SearchOption.TopDirectoryOnly));
+            if (imagesList.Count < 1)
+            {
+                MessageBox.Show("Votre dossier ne contient pas d'image ou pas le bon type d'image. (png et jpg only)");
+                return false; ;
+            }
+            foreach (string s in imagesList)
+            {
+                _pictures.Add(Image.FromFile(s));
+                _picturePath.Add(s);
+            }
+            return true;
+        }
+
+        private void btnUnlike_Click(object sender, EventArgs e)
+        {
+            if (_currentPic != 0)
+            {
+                _currentPic--;
+                UpdatePic();
+            }
+            else
+            {
+                MessageBox.Show("DIS IS ZE BEGINING (joy face)");
+            }
+        }
+
+        private void UpdatePic()
+        {
+            pbWallpaper.Image = _pictures[_currentPic];
+            pbProgression.Value = (_currentPic * 100) / (_pictures.Count - 1);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (_currentPic != _pictures.Count - 1)
+            {
+                _currentPic++;
+                UpdatePic();
+            }
+            else
+            {
+                MessageBox.Show("DIS IS ZE END (sad face)");
+            }
+        }
+
+        private void btnLike_Click(object sender, EventArgs e)
+        {
+            if (!_likeds.Contains(_picturePath[_currentPic]))
+            {
+                _likeds.Add(_picturePath[_currentPic]);
+                btnNext_Click(null, null);
+            }
+            else
+            {
+                MessageBox.Show("If u like too much u leak them");
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Themepack | *.themepack";
+            saveFileDialog.Title = "Enregistrer";
+            saveFileDialog.ShowDialog();
+            string fileName = new FileInfo(saveFileDialog.FileName).Name;
+            fileName = fileName.Replace(".themepack", "");
+
+            CreateDirectory("temp");//crée un dossier temporaire
+            CopyQuick();
+
+
+            //read and replace
+            string text = File.ReadAllText("temp\\super.theme");
+            text = text.Replace("DisplayName=Tinderspirobot", "DisplayName=" + fileName);
+            File.WriteAllText("temp\\super.theme", text);
+
+            CabInfo cab = new CabInfo(saveFileDialog.FileName);
+            cab.Pack("temp", true, Microsoft.Deployment.Compression.CompressionLevel.Normal, null);
+            Directory.Delete("temp", true);
+            Environment.Exit(7);
+
+        }
+
+        private void CopyQuick()
+        {
+            foreach (string path in _likeds)
+            {
+                File.Copy(path, "temp\\DesktopBackground\\" + new FileInfo(path).Name);
+            }
+
+            File.Copy("Resources\\super.theme", "temp\\super.theme", true);
+            File.Copy("Resources\\icon.png", "temp\\icon.png", true);
+        }
+
+        private void CreateDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+            DirectoryInfo di = Directory.CreateDirectory(path);
+            DirectoryInfo dii = Directory.CreateDirectory(path + "\\DesktopBackground");
+        }
+    }
+}
